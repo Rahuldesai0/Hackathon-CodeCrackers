e35add52aee34ba6f198883c2b2a056cd2989d60
Rahuldesai <84022777+Rahuldesai0@users.noreply.github.com>
2023-09-19 23:13:55 +0530

    hello.js 19-9-23

--- hello.js
+++ hello.js
@@ -1,5 +1,13 @@
 var icon = document.querySelector(".icon");
+var act = false;
               icon.addEventListener("click", function(){
                 document.querySelector("body").classList.toggle("active");
+                if(!act){
+                window.scrollBy(0, 300);
+                act = true;
+              }
+              else{
+                window.scrollBy(0, -300);
+                act = false;
+              }
               });
-
